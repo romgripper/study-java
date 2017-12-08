992d284d819e67fdafb55dcd891b6346f73c86ab
bin.xing <bin.xing@veritas.com>
2017-12-08 15:18:51 +0800

    [tryjdbc] format the source codes

--- tryjdbc/src/JDBCExample.java
+++ tryjdbc/src/JDBCExample.java
@@ -1,40 +1,51 @@
-import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JDBCExample {
+public class JDBCExample
+{
 
-  public static void main(String[] argv) {
+  public static void main(String[] argv)
+  {
 
-  System.out.println("-------- MySQL JDBC Connection Testing ------------");
+    System.out.println("-------- MySQL JDBC Connection Testing ------------");
 
-  try {
-    Class.forName("com.mysql.jdbc.Driver");
-  } catch (ClassNotFoundException e) {
-    System.out.println("Where is your MySQL JDBC Driver?");
-    e.printStackTrace();
-    return;
-  }
+    try
+    {
+      Class.forName("com.mysql.jdbc.Driver");
+    }
+    catch (ClassNotFoundException e)
+    {
+      System.out.println("Where is your MySQL JDBC Driver?");
+      e.printStackTrace();
+      return;
+    }
 
-  System.out.println("MySQL JDBC Driver Registered!");
-  Connection connection = null;
+    System.out.println("MySQL JDBC Driver Registered!");
+    Connection connection = null;
 
-  try {
-    connection = DriverManager
-    .getConnection("jdbc:mysql://10.192.5.126:3306/employees?useSSL=false","root", "xingbin");
-    //.getConnection("jdbc:mysql://localhost/feedback",
-    //      "sqluser", "xingbin");
+    try
+    {
+      connection = DriverManager
+          .getConnection("jdbc:mysql://10.192.5.126:3306/employees?useSSL=false", "root", "xingbin");
+      // .getConnection("jdbc:mysql://localhost/feedback",
+      // "sqluser", "xingbin");
 
-  } catch (SQLException e) {
-    System.out.println("Connection Failed! Check output console");
-    e.printStackTrace();
-    return;
-  }
+    }
+    catch (SQLException e)
+    {
+      System.out.println("Connection Failed! Check output console");
+      e.printStackTrace();
+      return;
+    }
 
-  if (connection != null) {
-    System.out.println("You made it, take control your database now!");
-  } else {
-    System.out.println("Failed to make connection!");
-  }
+    if (connection != null)
+    {
+      System.out.println("You made it, take control your database now!");
+    }
+    else
+    {
+      System.out.println("Failed to make connection!");
+    }
   }
 }
