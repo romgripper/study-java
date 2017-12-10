4c893d7fc96a3a2d9f26e0140977c9f787ba11b0
romgripper <romgripper@gmail.com>
2017-12-10 22:26:47 +0800

    [tryjdbc] try the try-with-resource idiom and AutoCloseable of Java 7

--- tryjdbc/src/JDBCExample.java
+++ tryjdbc/src/JDBCExample.java
@@ -9,17 +9,23 @@ import java.util.List;
 
 public class JDBCExample {
 
-	static class MusicDb {
+	private static void printException(Exception e) {
+		System.out.println(e.getMessage());
+		for (Throwable ex : e.getSuppressed()) {
+			System.out.println("Supressed: " + ex.getMessage());
+		}
+		e.printStackTrace();
+	}
+
+	static class MusicDb implements AutoCloseable {
 		private Connection connection;
-		private PreparedStatement insertArtistStatement;
-		private PreparedStatement nameByIdStatement;
 
 		static {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
 				System.out.println("JDBC driver not found");
-				e.printStackTrace();
+				printException(e);
 				System.exit(1);
 			}
 		}
@@ -34,83 +40,87 @@ public class JDBCExample {
 			}
 		}
 
+		@Override
 		public void close() throws SQLException {
-			if (insertArtistStatement != null) {
-				insertArtistStatement.close();
-			}
-			if (nameByIdStatement != null) {
-				nameByIdStatement.close();
-			}
+			System.out.println("MusicDb auto closed");
 			connection.close();
 		}
 
-		public void createTable() throws SQLException {
-			Statement s = connection.createStatement();
-			s.execute("DROP TABLE IF EXISTS artist");
-			System.out.println("Drop table artist successfully");
-			s.execute("CREATE TABLE artist (" + "id SMALLINT(5) NOT NULL AUTO_INCREMENT," + "name CHAR(20) NOT NULL,"
-					+ "PRIMARY KEY (id)," + "KEY by_name (name))");
-			System.out.println("Table artist is created successfully");
-			s.close();
+		public void createTable() {
+			try (Statement s = connection.createStatement()) {
+				s.execute("DROP TABLE IF EXISTS artist");
+				System.out.println("Drop table artist successfully");
+				s.execute("CREATE TABLE artist (" + "id SMALLINT(5) NOT NULL AUTO_INCREMENT,"
+						+ "name CHAR(20) NOT NULL," + "PRIMARY KEY (id)," + "KEY by_name (name))");
+				System.out.println("Table artist is created successfully");
+			} catch (SQLException e) {
+				printException(e);
+			}
 		}
 
-		public void insertArtist(String name) throws SQLException {
-			if (insertArtistStatement == null) {
-				insertArtistStatement = connection.prepareStatement("INSERT INTO artist (name) VALUES (?)");
+		public void insertArtist(String name) {
+			try (PreparedStatement s = connection.prepareStatement("INSERT INTO artist (name) VALUES (?)")) {
+				s.setString(1, name);
+				s.executeUpdate();
+			} catch (SQLException e) {
+				printException(e);
 			}
-			insertArtistStatement.setString(1, name);
-			insertArtistStatement.executeUpdate();
 			System.out.printf("%s are inserted successfully\n", name);
 		}
-		
-		public void insertArtists(List<String> names) throws SQLException {
+
+		public void insertArtists(List<String> names) {
 			StringBuffer sql = new StringBuffer();
 			sql.append("INSERT INTO artist (name) VALUES");
-			for (int i = 0; i < names.size(); i ++) {
+			for (int i = 0; i < names.size(); i++) {
 				sql.append("(?),");
 			}
 			sql.deleteCharAt(sql.length() - 1);
-			PreparedStatement s = connection.prepareStatement(sql.toString());
-			for (int i = 1; i <= names.size(); i ++) {
-				s.setString(i, names.get(i - 1));
+			try (PreparedStatement s = connection.prepareStatement(sql.toString())) {
+				for (int i = 1; i <= names.size(); i++) {
+					s.setString(i, names.get(i - 1));
+				}
+				s.executeUpdate();
+				System.out.println("Artists are inserted successfully");
+			} catch (SQLException e) {
+				printException(e);
 			}
-			s.executeUpdate();
-			System.out.println("Artists are inserted successfully");
-			s.close();
 		}
-		
-		public void listArtists() throws SQLException {
-			Statement s = connection.createStatement();
-			ResultSet results = s.executeQuery("SELECT * FROM artist ORDER BY id");
-			System.out.printf("%-10s  %s\n", "ID", "NAME");
-			System.out.printf("%-10s  %s\n", "--", "----");		
-			while (results.next()) {
-				String id = results.getString("id");
-				String name = results.getString("name");
-				System.out.printf("%-10s  %s\n", id, name);
+
+		public void listArtists() {
+			try (Statement s = connection.createStatement()) {
+				ResultSet results = s.executeQuery("SELECT * FROM artist ORDER BY id");
+				System.out.printf("%-10s  %s\n", "ID", "NAME");
+				System.out.printf("%-10s  %s\n", "--", "----");
+				while (results.next()) {
+					String id = results.getString("id");
+					String name = results.getString("name");
+					System.out.printf("%-10s  %s\n", id, name);
+				}
+			} catch (SQLException e) {
+				printException(e);
 			}
-			s.close();
 		}
-		
-		public boolean search(String name) throws SQLException {
+
+		public boolean search(String name) {
 			return false;
 		}
-		
-		public String getNameById(int id) throws SQLException {
-			PreparedStatement s = connection.prepareStatement("SELECT name FROM artist WHERE id = ?");
-			s.setInt(1, id);
-			ResultSet ret = s.executeQuery();
-			if (ret.next()) {
-				return ret.getString("name");
+
+		public String getNameById(int id) {
+			try (PreparedStatement s = connection.prepareStatement("SELECT name FROM artist WHERE id = ?")) {
+				s.setInt(1, id);
+				ResultSet ret = s.executeQuery();
+				if (ret.next()) {
+					return ret.getString("name");
+				}
+			} catch (SQLException e) {
+				printException(e);
 			}
 			return null;
 		}
 	}
 
 	public static void main(String[] argv) {
-		MusicDb db = null;
-		try {
-			db = new MusicDb();
+		try (MusicDb db = new MusicDb()) {
 			db.createTable();
 			db.insertArtist("John");
 			db.insertArtist("Tom");
@@ -124,17 +134,9 @@ public class JDBCExample {
 			System.out.printf("%d: %s\n", 2, db.getNameById(2));
 			System.out.printf("%d: %s\n", 7, db.getNameById(7));
 		} catch (SQLException e) {
-			e.printStackTrace();
+			printException(e);
 			System.exit(1);
-		} finally {
-			if (db != null) {
-				try {
-					db.close();
-				} catch (SQLException e) {
-					System.out.println(e.getMessage());
-				}
-			}
 		}
-
 	}
+
 }
