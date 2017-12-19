b0ace48825038634440f07a7c75929066e411183
romgripper <romgripper@gmail.com>
2017-12-20 00:19:18 +0800

    Try session.get(class, id)

--- HibernateExample/src/main/java/com/bingo/App.java
+++ HibernateExample/src/main/java/com/bingo/App.java
@@ -12,32 +12,37 @@ import com.bingo.util.HibernateUtil;
 public class App {
   public static void main(String[] args) {
     System.out.println("Maven + Hibernate + MySQL");
-    Session session = HibernateUtil.getSessionFactory().openSession();
+    try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
-    //session.beginTransaction();
-    Transaction transaction = session.beginTransaction();
-    DBUser user = new DBUser(100, "superman", "system", new Date());
-    session.saveOrUpdate(user);
-    user = new DBUser(101, "superwoman", "system", new Date());
-    session.saveOrUpdate(user);
-    user = new DBUser(102, "superson", "superwoman", new Date());
-    session.saveOrUpdate(user);
-    user = new DBUser(103, "superdaughter", "superwoman", new Date());
-    session.saveOrUpdate(user);
-    transaction.commit();
+      //session.beginTransaction();
+      Transaction transaction = session.beginTransaction();
+      DBUser user = new DBUser(100, "superman", "system", new Date());
+      session.saveOrUpdate(user);
+      user = new DBUser(101, "superwoman", "system", new Date());
+      session.saveOrUpdate(user);
+      user = new DBUser(102, "superson", "superwoman", new Date());
+      session.saveOrUpdate(user);
+      user = new DBUser(103, "superdaughter", "superwoman", new Date());
+      session.saveOrUpdate(user);
+      transaction.commit();
 
-    transaction = session.beginTransaction();
-    user.setCreatedBy("superman");
-    session.update(user);
-    // The DBUser after "from" is the class name
-    List<DBUser> users = (List<DBUser>) session.createQuery("from DBUser").list();
-    for (DBUser u : users) {
-      System.out.println(u);
+      transaction = session.beginTransaction();
+      user.setCreatedBy("superman");
+      session.update(user);
+      // The DBUser after "from" is the class name
+      List<DBUser> users = (List<DBUser>) session.createQuery("from DBUser").list();
+      for (DBUser u : users) {
+        System.out.println(u);
+      }
+      // Unable to locate persister: BUSER
+      //System.out.println(session.get("BUSER", 100));
+      System.out.println(session.get(DBUser.class, 101));
+
+      //session.getTransaction().commit();
+      transaction.commit();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
     }
-
-    //session.getTransaction().commit();
-    transaction.commit();
-    session.close();
 
     HibernateUtil.shutdown();
   }
