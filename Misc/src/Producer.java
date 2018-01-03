9c15f593c24572ff66cf411a27ad7c8224b8ba1a
bin.xing <xb1101101@gmail.com>
2018-01-03 15:50:47 +0800

    [Misc] Retry thread

--- Misc/src/Producer.java
+++ Misc/src/Producer.java
@@ -2,44 +2,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Producer implements Runnable {
-	static final int MAXQUEUE = 5;
-	private List messages = new ArrayList();
+  static final int MAXQUEUE = 5;
+  private List<String> messages = new ArrayList<>();
+  private int id = 0;
 
-	public void run() {
-		while (true) {
-			String date = putMessage();
-			// System.out.println("Put message: " + date);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-			}
-		}
-	}
+  public void run() {
+    while (true) {
+      String message = putMessage();
+      System.out.println("Put message: " + message);
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+      }
+    }
+  }
 
-	// called by Producer internally
-	private synchronized String putMessage() {
-		while (messages.size() >= MAXQUEUE)
-			try {
-				//notifyAll();
-				wait();
-			} catch (InterruptedException e) {
-			}
-		String date = new java.util.Date().toString();
-		messages.add(date);
-		notifyAll();
-		return date;
-	}
+  // called by Producer internally
+  private synchronized String putMessage() {
+    while (messages.size() >= MAXQUEUE)
+      try {
+        wait();
+      } catch (InterruptedException e) {
+      }
+    String message = "" + (id++) + ": ";
+    message += new java.util.Date().toString();
+    messages.add(message);
+    notifyAll();
+    return message;
+  }
 
-	// called by Consumer externally
-	public synchronized String getMessage() {
-		while (messages.size() == 0)
-			try {
-				//notifyAll();
-				wait();
-			} catch (InterruptedException e) {
-			}
-		String message = (String) messages.remove(0);
-		notifyAll();
-		return message;
-	}
+  // called by Consumer externally
+  public synchronized String getMessage() {
+    while (messages.size() == 0)
+      try {
+        wait();
+      } catch (InterruptedException e) {
+      }
+    String message = messages.remove(0);
+    notifyAll();
+    return message;
+  }
 }
