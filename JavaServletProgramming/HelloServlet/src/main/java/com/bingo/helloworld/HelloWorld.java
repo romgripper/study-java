d58e44c5d1a64a3eca9ea61fec172156a657ccd3
romgripper <romgripper@gmail.com>
2018-01-16 22:04:50 +0800

    [JavaServletProgramming/HelloServlet] Try to handle POST and HEAD

--- JavaServletProgramming/HelloServlet/src/main/java/com/bingo/helloworld/HelloWorld.java
+++ JavaServletProgramming/HelloServlet/src/main/java/com/bingo/helloworld/HelloWorld.java
@@ -9,19 +9,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorld extends HttpServlet {
+
+  @Override
   public void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException {
+
+    res.setContentType("text/html");
+    if (req.getMethod().equals("HEAD")) {
+      return;
+    }
+
+    String name = req.getParameter("name");
+    if (name != null) {
+      doPost(req, res);
+      return;
+    }
+
+    PrintWriter out = res.getWriter();
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head><title>Hello</title></head>");
+    out.println("<body>");
+    out.println(
+        "<form method='post' action='/HelloServlet/hello'>"
+            + "If you don&quot;t mind me asking, what is your name?"
+            + "<input type='text' name='name'><p>"
+            + "<input type='submit' value='Submit'></p>"
+            + "</form>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse res)
       throws ServletException, IOException {
     res.setContentType("text/html");
     PrintWriter out = res.getWriter();
 
-    String url = req.getRequestURI();
+    String name = req.getParameter("name");
 
     out.println("<!DOCTYPE html>");
     out.println("<html>");
-    out.println("<head><title>Hello World</title></head>");
-    out.println("<body><h1>Hello World</h1>");
-    out.println("request url: " + url);
+    out.println("<head><title>Hello " + name + "</title></head>");
+    out.println("<body><h1>Hello " + name + "</h1>");
     out.println("</body>");
     out.println("</html>");
+  }
+
+  @Override
+  public String getServletInfo() {
+    return "A servlet that knows the name of the person to whom it's saying hello";
   }
 }
